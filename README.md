fe9400a2a93bde18eaea87b925ffb8d2977aa714
Alex Ausch <alex.ausch@going.com>
2025-02-05 14:13:39 -0500

    Repo setup and flux-vaidate-repo.sh hook

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# flux_validate_precommit_hook
-experimental
+# Fetches then runs the validate.sh script provided by the flux folk against the entire repo
+
+Credit: Adapted for this purpose from https://github.com/TekWizely/pre-commit-golang/
+
+WARNING: experimental/wip
---
+++ flux-validate-repo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+error_on_output=1
+FILE=deps/validate.sh
+if [ ! -f "$FILE" ]; then
+    mkdir -p deps
+    curl -sL https://github.com/fluxcd/flux2-kustomize-helm-example/blob/main/scripts/validate.sh > FILE"
+fi
+cmd=(deps/validate.sh)
+target=(.)
+. "$(dirname "${0}")/lib/cmd-repo.bash"
