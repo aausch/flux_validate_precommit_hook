29d49bcd01554a8beddd287ec38d68065946c0cc
Alex Ausch <alex.ausch@going.com>
2025-02-05 14:25:53 -0500

    Mismatched quotations

--- run-flux-validate-repo.sh
+++ run-flux-validate-repo.sh
@@ -3,7 +3,7 @@ error_on_output=1
 FILE=deps/validate.sh
 if [ ! -f "$FILE" ]; then
     mkdir -p deps
-    curl -sL https://github.com/fluxcd/flux2-kustomize-helm-example/blob/main/scripts/validate.sh > FILE"
+    curl -sL https://github.com/fluxcd/flux2-kustomize-helm-example/blob/main/scripts/validate.sh > "$FILE"
 fi
 cmd=(deps/validate.sh)
 target=(.)
