75f783a0ee310a19b2183887894d88533dfbc853
Alex Ausch <alex.ausch@going.com>
2025-02-05 14:36:28 -0500

    Download validate.sh to tmp directory instead

--- run-flux-validate-repo.sh
+++ run-flux-validate-repo.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+
 error_on_output=1
-FILE=deps/validate.sh
+FILE=/tmp/flux-validate/validate.sh
 if [ ! -f "$FILE" ]; then
-    mkdir -p deps
+    mkdir -p /tmp/flux-validate/
     curl -sL https://github.com/fluxcd/flux2-kustomize-helm-example/blob/main/scripts/validate.sh > "$FILE"
 fi
-cmd=(deps/validate.sh)
+cmd=("$FILE")
 target=(.)
 . "$(dirname "${0}")/lib/cmd-repo.bash"
