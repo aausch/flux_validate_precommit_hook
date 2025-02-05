bf094c69e44bd245338aea5c248841b6c54d7eb5
Alex Ausch <alex.ausch@going.com>
2025-02-05 15:53:25 -0500

    Linted

--- run-flux-validate-repo.sh
+++ run-flux-validate-repo.sh
@@ -2,9 +2,9 @@
 
 FILE=/tmp/flux-validate-precommit-hook/validate.sh
 if [[ ! -f "${FILE}" ]]; then
-       mkdir -p /tmp/flux-validate-precommit-hook
-       curl -sL https://raw.githubusercontent.com/fluxcd/flux2-kustomize-helm-example/refs/heads/main/scripts/validate.sh > "${FILE}"
-       chmod +x "${FILE}"
+	mkdir -p /tmp/flux-validate-precommit-hook
+	curl -sL https://raw.githubusercontent.com/fluxcd/flux2-kustomize-helm-example/refs/heads/main/scripts/validate.sh > "${FILE}"
+	chmod +x "${FILE}"
 fi
 cmd=("${FILE}")
 target=("> /dev/null")
